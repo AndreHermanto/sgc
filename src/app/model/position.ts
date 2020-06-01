bf92606fb446917b1309774dd36fc4c5ef812322
Andre Hermanto <andre.hermanto93@gmail.com>
2020-06-02 08:00:09 +1000

    Change position to single coordinate

--- src/app/model/position.ts
+++ src/app/model/position.ts
@@ -2,7 +2,7 @@ import { Region } from './region';
 
 export class Position extends Region {
     constructor(c: string, public position: number) {
-        super(c, position <= 50 ? 0 : position - 50, position <= 50 ? 50 : position + 50);
+        super(c, position, position);
     }
 
     name() {
